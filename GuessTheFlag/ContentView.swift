c76a976969c3a36ad45ff1a30a5b15c153d52148
Dechon <72828058+dech0n@users.noreply.github.com>
2024-05-01 12:43:45 -0700

    Make flag buttons show score when tapped

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     var correctAnswer = Int.random(in: 0...2)
     
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     
     var body: some View {
         ZStack {
@@ -28,13 +31,18 @@ struct ContentView: View {
                 
                 ForEach(0..<3) { number in
                     Button {
-                        // flag was tapped
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                     }
                 }
             }
         }
+    }
+    
+    func flagTapped(_ number: Int) {
+        scoreTitle = number == correctAnswer ? "Correct" : "Wrong"
+        showingScore = true
     }
 }
 
